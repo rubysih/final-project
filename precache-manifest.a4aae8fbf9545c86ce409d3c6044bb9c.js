2253aa53bfe57fdae3d3597f09bdedf2e632538d
rubysih <rr910052@gmail.com>
2019-01-06 15:16:45 +0800

    Add files via upload

---
+++ precache-manifest.a4aae8fbf9545c86ce409d3c6044bb9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ec8d5871356da092ae135bc01581028",
+    "url": "./static/media/product-1.4ec8d587.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a42b1cadbe593fcd129d",
+    "url": "./static/js/main.a42b1cad.chunk.js"
+  },
+  {
+    "revision": "04aa21e1b751ba3f056b",
+    "url": "./static/js/1.04aa21e1.chunk.js"
+  },
+  {
+    "revision": "a42b1cadbe593fcd129d",
+    "url": "./static/css/main.b477229d.chunk.css"
+  },
+  {
+    "revision": "feb4d422e2e51712dcea0f4c7834435d",
+    "url": "./index.html"
+  }
+];
